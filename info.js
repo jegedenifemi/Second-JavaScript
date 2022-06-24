5a3d5949c27500d4105f9c2bb3a04c4d4c0bbc4f
jegedenifemi <jegedenifemi24@gmail.com>
2022-06-24 10:25:15 +0100

    initial commit

---
+++ info.js
@@ -0,0 +1,7 @@
+let Name = "Nifemi";
+let Height = "174cm";
+let Country = "Nigeria"
+alert("My name is " + Name + ".")
+alert("I am " + Height + " in height.")
+alert("I am from " + Country + ".")
+
